b098af412864db92b83fad8749a7a7ae01628067
Tristan Ross <tristan.ross@midstall.com>
2023-08-17 12:41:57 -0700

    fix(main): remove unneded things

--- main.tf
+++ main.tf
@@ -5,9 +5,7 @@ terraform {
     key = "infra/terraform.tfstate"
     endpoint = "https://s3.wasabisys.com"
     region = "us-west-1"
-    skip_requesting_account_id = true
     skip_credentials_validation = true
-    skip_get_ec2_platforms = true
     skip_metadata_api_check = true
   }
 
