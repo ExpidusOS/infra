c02723f3e5b3bc8e2a14bbad5b031f049c02a6d6
Tristan Ross <tristan.ross@midstall.com>
2023-08-17 17:22:06 -0700

    feat(external-dns): added

---
+++ external-dns.tf
@@ -0,0 +1,32 @@
+resource "helm_release" "external-dns" {
+  repository = "https://charts.bitnami.com/bitnami"
+  chart = "external-dns"
+
+  namespace = "external-dns"
+  create_namespace = true
+
+  set {
+    name = "cloudflare.apiToken"
+    value = var.cloudflare_token
+  }
+
+  set {
+    name = "cloudflare.apiKey"
+    value = var.cloudflare_key
+  }
+
+  set {
+    name = "cloudflare.email"
+    value = "rosscomputerguy@protonmail.com"
+  }
+}
+
+variable "cloudflare_token" {
+  type = string
+  sensitive = true
+}
+
+variable "cloudflare_key" {
+  type = string
+  sensitive = true
+}
