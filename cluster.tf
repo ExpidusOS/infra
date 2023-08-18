18923043a3de8b651f5e03fe612f063dd5c1d483
Tristan Ross <tristan.ross@midstall.com>
2023-08-18 09:15:22 -0700

    feat(#45): add cluster config

--- cluster.tf
+++ cluster.tf
@@ -1,18 +1,75 @@
-data "google_container_cluster" "infra" {
+data "google_container_engine_versions" "gke_version" {
+  location = var.region
+  version_prefix = "1.27."
+}
+
+data "google_service_account" "default" {
+  account_id = "github-ci"
+}
+
+resource "google_compute_network" "infra-vpc" {
+  name = "infra-${var.region}-vpc"
+  auto_create_subnetworks = "false"
+}
+
+resource "google_compute_subnetwork" "infra-subnet" {
+  name = "infra-${var.region}-subnet"
+  region = var.region
+  network = resource.google_compute_network.infra-vpc.name
+  ip_cidr_range = "10.10.0.0/24"
+}
+
+resource "google_container_cluster" "infra" {
   name = "infra-${var.region}"
   location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count = 1
+
+  network = resource.google_compute_network.infra-vpc.name
+  subnetwork = resource.google_compute_subnetwork.infra-subnet.name
+}
+
+resource "google_container_node_pool" "infra-primary-nodes" {
+  name = resource.google_container_cluster.infra.name
+  location = var.region
+  cluster = resource.google_container_cluster.infra.name
+  
+  version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
+  node_count = 2
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
+
+  node_config {
+    service_account = resource.google_service_account.default.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    disk_type = "pb-standard"
+    disk_size_gb = 25
+
+    machine_type = "n1-standard-1"
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
 }
 
 provider "kubernetes" {
-  host = "https://${data.google_container_cluster.infra.endpoint}"
+  host = "https://${resource.google_container_cluster.infra.endpoint}"
   token = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.infra.master_auth[0].cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(resource.google_container_cluster.infra.master_auth[0].cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
-    host = "https://${data.google_container_cluster.infra.endpoint}"
+    host = "https://${resource.google_container_cluster.infra.endpoint}"
     token = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(data.google_container_cluster.infra.master_auth[0].cluster_ca_certificate)
+    cluster_ca_certificate = base64decode(resource.google_container_cluster.infra.master_auth[0].cluster_ca_certificate)
   }
 }
